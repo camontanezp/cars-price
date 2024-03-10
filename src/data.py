83e7c5a3be9f2dbe316d8e9a0e07af97abc52908
Ciro Montañez <ciroalfonsom@microsoft.com>
2024-03-10 17:03:03 -0500

    Add data split and category conversion

--- src/data.py
+++ src/data.py
@@ -1,3 +1,6 @@
+from sklearn.model_selection import train_test_split
+
+
 def get_season_us_by_month(month):
     if month in [12, 1, 2]:
         return "winter"
@@ -9,3 +12,15 @@ def get_season_us_by_month(month):
         return "autumn"
     else:
         return "error"
+    
+def convert_object_columns_to_category(data):
+    object_columns = data.select_dtypes(include='object').columns
+    for column in object_columns:
+        data[column] = data[column].astype('category')
+    return data
+
+def get_train_test_data(data, features, target, test_size=0.2):
+    X = data[features]
+    y = data[target]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    return X_train, X_test, y_train, y_test
