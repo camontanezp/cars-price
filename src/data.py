bd87f2e3df1698d24d6ef1e84e86f8107b00c7a6
Ciro Montañez <ciroalfonsom@microsoft.com>
2024-03-10 13:58:35 -0500

    Add data trans functions

---
+++ src/data.py
@@ -0,0 +1,11 @@
+def get_season_us_by_month(month):
+    if month in [12, 1, 2]:
+        return "winter"
+    elif month in [3, 4, 5]:
+        return "spring"
+    elif month in [6, 7, 8]:
+        return "summer"
+    elif month in [9, 10, 11]:
+        return "autumn"
+    else:
+        return "error"
