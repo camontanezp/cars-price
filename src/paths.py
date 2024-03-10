538b028dd0dd8499e191fc8b3e8f649baeb7d4d3
Ciro Montañez <ciroalfonsom@microsoft.com>
2024-03-10 11:48:57 -0500

    Add paths file

--- src/paths.py
+++ src/paths.py
@@ -4,6 +4,7 @@ import os
 PARENT_DIR = Path(__file__).parent.resolve().parent
 DATA_DIR = PARENT_DIR / 'data'
 RAW_DATA_DIR = DATA_DIR /'raw'
+CLEANED_DATA_DIR = DATA_DIR / 'cleaned'
 TRANSFORMED_DATA_DIR = DATA_DIR / 'transformed'
 MODELS_DIR = PARENT_DIR / 'models'
 
@@ -12,6 +13,9 @@ if not Path(DATA_DIR).exists():
 
 if not Path(RAW_DATA_DIR).exists():
     os.mkdir(RAW_DATA_DIR)
+
+if not Path(CLEANED_DATA_DIR).exists():
+    os.mkdir(CLEANED_DATA_DIR)
 
 if not Path(TRANSFORMED_DATA_DIR).exists():
     os.mkdir(TRANSFORMED_DATA_DIR) 
