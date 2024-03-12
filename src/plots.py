e20a57f46e8f59a03587482e7d981eb97a3e34cd
Ciro Montañez <ciroalfonsom@microsoft.com>
2024-03-12 11:01:06 -0500

    Add shap plots

--- src/plots.py
+++ src/plots.py
@@ -1,4 +1,8 @@
 import plotly.express as px 
+import shap
+
+# print the JS visualization code to the notebook
+shap.initjs()
 
 
 def plot_avg_target_time_series_by_feature(data, feature, target='price'):
@@ -35,3 +39,30 @@ def plot_scatter_of_target_vs_feature(data, feature, target='price'):
 def plot_scatter_of_target_vs_features(data, features, target='price'):
     for feature in features:
         plot_scatter_of_target_vs_feature(data, feature, target='price')
+
+def plot_shap_summary(metadata, model, X):
+    if 'XGBoost linear' in metadata["name"]:
+        print('Linear model detected. Shap explanation not supported.')
+    elif 'Linear regression model' in metadata["name"]:
+        explainer = shap.LinearExplainer(model, X)
+        shap_values = explainer.shap_values(X)
+        shap.summary_plot(shap_values, X, feature_names=metadata['features'], plot_type='bar')
+    elif 'XGBoost with trees' in metadata["name"]:
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(X)
+        shap.summary_plot(shap_values, X, feature_names=metadata['features'], plot_type='bar')
+    else:
+        raise ValueError('Model type not supported')
+    
+    return explainer
+
+def plot_shap_waterfall_for_car_sample(metadata, explainer, X_for_shap, car_index):
+    if 'XGBoost linear' in metadata["name"]:
+        print('Linear model detected. Shap explanation not supported.')
+        plot = None
+    elif 'Linear regression model' in metadata["name"] or 'XGBoost with trees' in metadata["name"]:
+        shap_values = explainer(X_for_shap)
+        plot = shap.plots.waterfall(shap_values[car_index])
+    else:
+        raise ValueError('Model type not supported')
+    
